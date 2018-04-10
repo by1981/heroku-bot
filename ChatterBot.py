4ba67b82bd8017320a42253262711f642744971c
Bhavani Yella <bhavaniyella@icloud.com>
2018-04-09 21:36:22 -0700

    6

--- ChatterBot.py
+++ ChatterBot.py
@@ -4,10 +4,11 @@ import json
 import time
 
 # Twitter API Keys
-consumer_key = "Ed4RNulN1lp7AbOooHa9STCoU"
-consumer_secret = "P7cUJlmJZq0VaCY0Jg7COliwQqzK0qYEyUF9Y0idx4ujb3ZlW5"
-access_token = "839621358724198402-dzdOsx2WWHrSuBwyNUiqSEnTivHozAZ"
+consumer_key = "ImLGT5Pbebk33jYv76ozNt5KB"
+consumer_secret = "alFm0kHzJ3IFhDlNn5Z4ifX4JpzANpqxFgWxHbs4Ewv2kwd8tc"
+access_token = "4876379418-qNp9F4td5lyyefVcE3Q7eNNNiJtFDTClHncUuRt"
 access_token_secret = "dCZ80uNRbFDjxdU2EckmNiSckdoATach6Q8zb7YYYE5ER"
+
 
 # Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
